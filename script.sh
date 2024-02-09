c11341541fce364bd167757ab2a818c69e065f4d
betalantz <betalantz@gmail.com>
2024-02-09 11:37:52 -0800

    modify sh and README

--- script.sh
+++ script.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # argumentvariables
-secret_key=`hexdump -vn16 -e'4/4 "%08X" 1 "\n"' /dev/urandom`
+# secret_key=`hexdump -vn16 -e'4/4 "%08X" 1 "\n"' /dev/urandom`
 
 # setup virtual environment and add dependencies
-pipenv install --python 3.9.2
+pipenv install --python 3.8.13
 
 # install python packages
-pipenv install flask flask-sqlalchemy sqlalchemy-serializer flask-cors flask-restful flask-migrate flask-bcrypt python-dotenv
+pipenv install flask flask-sqlalchemy flask-restful flask-migrate python-dotenv ipdb
 
 # filestructures
 mkdir server
 touch server/app.py
+touch server/models.py
 touch server/config.py
 touch server/seed.py
+touch server/debug.py
 touch .env
 touch .gitignore
 
@@ -22,9 +24,6 @@ echo "from flask import Flask" >> server/config.py
 echo "from flask_sqlalchemy import SQLAlchemy" >> server/config.py
 echo "from sqlalchemy import MetaData" >> server/config.py
 echo "from flask_migrate import Migrate" >> server/config.py
-echo "from flask_bcrypt import Bcrypt" >> server/config.py
-echo "from flask_restful import Api" >> server/config.py
-echo "from flask_cors import CORS" >> server/config.py
 echo "from dotenv import load_dotenv" >> server/config.py
 echo -e "import os\n" >> server/config.py
 
@@ -40,71 +39,60 @@ echo -e "naming_convention = {
 }\n" >> server/config.py
 echo -e "metadata = MetaData(naming_convention=naming_convention)\n" >> server/config.py
 echo "app = Flask(__name__)" >> server/config.py
-echo "app.secret_key = os.getenv(\"FLASK_SECRET_KEY\")" >> server/config.py
 echo "app.config[\"SQLALCHEMY_DATABASE_URI\"] = os.getenv(\"DATABASE_URI\")" >> server/config.py
 echo -e "app.config[\"SQLALCHEMY_TRACK_MODIFICATIONS\"] = False\n" >> server/config.py
 echo -e "db = SQLAlchemy(app=app, metadata=metadata)\n" >> server/config.py
 echo -e "migrate = Migrate(app=app, db=db)\n" >> server/config.py
-echo -e "bcrypt = Bcrypt(app=app)\n" >> server/config.py
-echo -e "api = Api(app=app)\n" >> server/config.py
-echo "CORS(app)" >> server/config.py
+
+# imports models.py
+echo -e "from config import db\n" >> server/models.py
 
 # imports app.py
-echo -e "from config import app\n" >> server/app.py
+echo -e "from config import app, migrate\n" >> server/app.py
+echo -e "from models import db\n" >> server/app.py
 
 # configuring app.py
 echo "if __name__ == \"__main__\":" >> server/app.py
-echo "  app.run(port=5555, debug=True)" >> server/app.py
+echo "  with app.app_context():" >> server/app.py
+echo "    migrate.init_app(app, db)" >> server/app.py
+echo "    pass" >> server/app.py
+echo "    # remove pass and write your cli logic" >> server/app.py
 
 # configuring seed.py
-echo -e "from config import app, db\n" >> server/seed.py
+echo -e "from config import app\n" >> server/seed.py
+echo -e "from models import *\n" >> server/seed.py
 echo "if __name__ == \"__main__\":" >> server/seed.py
 echo "  with app.app_context():" >> server/seed.py
 echo "    pass" >> server/seed.py
 echo "    # remove pass and write your seed data" >> server/seed.py
 
+# configuring debug.py
+echo -e "import sys\n" >> server/debug.py
+echo -e "import ipdb\n" >> server/debug.py
+echo -e "from config import app\n" >> server/debug.py
+echo -e "from models import *\n" >> server/debug.py
+echo "if __name__ == \"__main__\":" >> server/debug.py
+echo "  with app.app_context():" >> server/debug.py
+echo "    pass" >> server/debug.py
+echo "    # remove pass and write your debug implementation" >> server/debug.py
+echo "  ipdb.set_trace(sys._getframe())" >> server/debug.py
+
+
+
 # configuring .gitignore
 echo ".env" >> .gitignore
 
 # configuring .env
-echo "FLASK_SECRET_KEY=$secret_key" >> .env
+# echo "FLASK_SECRET_KEY=$secret_key" >> .env
 echo "DATABASE_URI=sqlite:///app.db" >> .env
 
 # initializing database
 cd server
+export FLASK_APP=app.py
 
 pipenv run flask db init
 pipenv run flask db revision -m 'initial db'
 pipenv run flask db upgrade head
-
-cd ..
-
-# creating react client
-npm create vite@latest client -- --template react
-cd client
-npm install react-router-dom formik yup
-
-# clear out vite configuration by removing file and re-adding
-rm vite.config.js
-touch vite.config.js
-
-# add new vite configuration
-
-echo "import { defineConfig } from 'vite'" >> vite.config.js
-echo -e "import react from '@vitejs/plugin-react'\n" >> vite.config.js
-echo "// https://vitejs.dev/config/" >> vite.config.js
-echo "export default defineConfig({" >> vite.config.js
-echo "  plugins: [react()]," >> vite.config.js
-echo "  server: {" >> vite.config.js
-echo "   proxy: {" >> vite.config.js
-echo "    '/api': {" >> vite.config.js
-echo "      target: 'http://localhost:5555'," >> vite.config.js
-echo "      changeOrigin: true," >> vite.config.js
-echo "      rewrite: (path) => path.replace(/^\/api/, '')," >> vite.config.js
-echo "    }," >> vite.config.js
-echo "   }," >> vite.config.js
-echo "  }" >> vite.config.js
-echo "})" >> vite.config.js
 
 cd ..
 
